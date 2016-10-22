dfa00f85f53d8960eefabaa6e7db04df082ede3d
Agustí Becerra Milà <agustibm@MacBook-MacBook-Pro-de-Agusti.local>
2016-10-22 10:45:11 +0200

    Is cell have two Neighbour life

--- src/cell.js
+++ src/cell.js
@@ -1,6 +1,21 @@
 class Cell{
+    constructor(){
+        this.life = false;
+    }
     isLive(){
-        return true;
+        return this.life;
+    }
+
+    check(neighbours) {
+        let alife = 0;
+        neighbours.forEach(function (element) {
+            if(element){
+                alife++;
+            }
+        });
+        if(alife > 1){
+            this.life = true;
+        }
     }
 }
 module.exports = Cell;
--- tests/unit/cell.spec.js
+++ tests/unit/cell.spec.js
@@ -1,9 +1,18 @@
 const assert = require('chai').assert;
 const Cell  = require('../../src/cell');
+const CORNERS = 6;
 
 suite('Unit Test - Cell', function () {
+    let cell;
+    before(() => {
+        cell = new Cell();
+    });
     test('Create a new cell is live', function () {
-        let cell = new Cell();
+        assert.equal(cell.isLive(), false);
+    });
+    test('Is cell have two Neighbour life', function () {
+        let neighbours = [true, true, false, false];
+        cell.check(neighbours);
         assert.equal(cell.isLive(), true);
     });
 });
