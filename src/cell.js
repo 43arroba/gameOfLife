8471b4f951a4bb558606630691aa488fbe9b9caa
Agustí Becerra Milà <agustibm@MacBook-MacBook-Pro-de-Agusti.local>
2016-10-22 11:05:21 +0200

    Reset

--- src/cell.js
+++ src/cell.js
@@ -1,23 +1,4 @@
 class Cell{
-    constructor(){
-        this.life = false;
-    }
-    isLive(){
-        return this.life;
-    }
 
-    check(neighbours) {
-        let alife = 0;
-        neighbours.forEach(function (element) {
-            if(element){
-                alife++;
-            }
-        });
-        if(alife > 1){
-            this.life = true;
-        }else{
-            this.life = false;
-        }
-    }
 }
 module.exports = Cell;
--- tests/unit/cell.spec.js
+++ tests/unit/cell.spec.js
@@ -3,27 +3,6 @@ const Cell  = require('../../src/cell');
 const CORNERS = 6;
 
 suite('Unit Test - Cell', function () {
-    let cell;
-    before(() => {
-        cell = new Cell();
-    });
 
-    suite('Rules cell', () => {
-        test('Create a new cell is live', function () {
-            assert.equal(cell.isLive(), false);
-        });
-
-        test('Is cell have two alvie neighbour is life', function () {
-            let neighbours = [true, true, false, false];
-            cell.check(neighbours);
-            assert.equal(cell.isLive(), true);
-        });
-
-        test('Is cell have one alive neighbour is dead',function () {
-            let neighbours = [true, false, false, false];
-            cell.check(neighbours);
-            assert.equal(cell.isLive(), false);
-        });
-    });
 });
 
