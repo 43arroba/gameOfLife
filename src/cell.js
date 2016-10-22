b812c5b36d875aefc75968aedb4b8c66088cb615
Agustí Becerra Milà <agustibm@MacBook-MacBook-Pro-de-Agusti.local>
2016-10-22 12:11:38 +0200

    2nd iteration

--- src/cell.js
+++ src/cell.js
@@ -1,4 +1,21 @@
 class Cell{
 
+    check(neightboards) {
+        let alive = 0;
+        neightboards.forEach((isAlive) => {
+            if(isAlive){
+                alive++;
+            }
+        });
+        
+        return { isAlive };
+        
+        function isAlive(){
+            if(alive === 2 || alive === 3){
+                return true;
+            }
+            return false;
+        }
+    }
 }
 module.exports = Cell;
--- tests/unit/cell.spec.js
+++ tests/unit/cell.spec.js
@@ -1,8 +1,43 @@
 const assert = require('chai').assert;
-const Cell  = require('../../src/cell');
-const CORNERS = 6;
+const Cell = require('../../src/cell');
+const CORNERS = 8;
 
 suite('Unit Test - Cell', function () {
+    let cell;
+
+    beforeEach(() => {
+        cell = new Cell();
+    });
+
+    suite('should live cell', () => {
+        test('the next generation state', function () {
+            let neightboardsTwoLive = [true, true, false, false, false, false, false, false];
+            let neightboardsThreeLive = [true, true, true, false, false, false, false, false];
+
+            assertAliveCell(cell.check(neightboardsTwoLive).isAlive());
+            assertAliveCell(cell.check(neightboardsThreeLive).isAlive());
+        });
+
+        function assertAliveCell(aliveCell) {
+            assert.equal(aliveCell, true);
+        }
+    });
+
+    suite('should dead cell', () => {
+        test('Creating a new cell with under-population', function () {
+            let neightboards = [true, false, false, false, false, false];
+            assertDeadCell(cell.check(neightboards).isAlive());
+        });
+
+        test('Creating a new cell with over-population', function () {
+            let neightboards = [true, true, true, true, false, false];
+            assertDeadCell(cell.check(neightboards).isAlive());
+        });
+
+        function assertDeadCell(aliveCell) {
+            assert.equal(aliveCell, false);
+        }
+    });
 
 });
 
